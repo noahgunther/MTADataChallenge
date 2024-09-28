327ddb311c36b0d101791f411536106d0c30a2ed
Noah Gunther <noah.gunther@gmail.com>
2024-09-28 15:22:39 -0400

    Adding draft text and aggregation functionality

--- script.js
+++ script.js
@@ -2,28 +2,48 @@ window.addEventListener("load", init, false);
 
 function init() {
 
-    function dateConstructor(subDays) {
-        const date = new Date();
-        date.setDate(date.getDate() - subDays);
-        let day = date.getDate().toString();
-        day.length < 2 ? day = '0' + day : day;
-        let month = (date.getMonth() + 1).toString();
-        month.length < 2 ? month = '0' + month : month;
-        let year = date.getFullYear();
-        return year + "-" + month + "-" + day;
+    function dateConstructor(subStart, range) {
+        const dates = [];
+        for (let i=0; i<range; i++) {
+            const date = new Date();
+            date.setDate(date.getDate() - subStart);
+            let day = date.getDate().toString();
+            day.length < 2 ? day = '0' + day : day;
+            let month = (date.getMonth() + 1).toString();
+            month.length < 2 ? month = '0' + month : month;
+            let year = date.getFullYear();
+            dates.push(year + "-" + month + "-" + day);
+            subStart++;
+        }
+        return dates;
     }
 
-    function loadData(element, date) {
+    function loadData(element, dates) {
+        let dateString = '';
+        for (let i=0; i<dates.length; i++) {
+            dateString += "date='" + dates[i];
+            if (i != dates.length-1) {
+                dateString += "' OR ";
+            }
+            else {
+                dateString += "'";
+            }
+        }
         const xhttp = new XMLHttpRequest();
         xhttp.onload = function() {
-            const response = JSON.parse(this.responseText)[0];
-            if (element = 'subway') document.getElementById(element).innerHTML = response.subways_total_estimated_ridership;
-            if (element = 'bus') document.getElementById(element).innerHTML = response.buses_total_estimated_ridersip;
+            const response = JSON.parse(this.responseText);
+            let totalRidership = 0;
+            for (let i=0; i<response.length; i++) {
+                if (element == 'subway') totalRidership += parseInt(response[i].subways_total_estimated_ridership);
+                else if (element == 'bus') totalRidership += parseInt(response[i].buses_total_estimated_ridersip);
+            }
+            if (element == 'subway') document.getElementById(element).innerHTML = totalRidership;
+            else if (element == 'bus') document.getElementById(element).innerHTML = totalRidership;
         }
-        xhttp.open("GET", "http://data.ny.gov/resource/vxuj-8kew.json?$limit=1&$$app_token=fIErfxuaUHt3vyktfOyK1XFRS&date=" + date);
+        xhttp.open("GET", "http://data.ny.gov/resource/vxuj-8kew.json?$$app_token=fIErfxuaUHt3vyktfOyK1XFRS&$where=" + dateString);
         xhttp.send();
-      }
+    }
 
-    loadData('subway', dateConstructor(2));
-    loadData('bus', dateConstructor(2));
+    loadData('subway', dateConstructor(5, 7));
+    loadData('bus', dateConstructor(5, 7));
 }
